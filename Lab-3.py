745f555ceddbc75541d63dbfc88335a5d8e8c2e4
jyoti-bhati <bhjyoti12@gmail.com>
2025-02-05 16:30:44 +0530

    Add files via upload

---
+++ Lab-3.py
@@ -0,0 +1,13 @@
+#3. Write a program to find the largest of 2 numbers.
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if num1 > num2:
+    print(f"The largest number is {num1}")
+else:
+    print(f"The largest number is {num2}")
+
+'''output
+Enter the first number: 20
+Enter the second number: 13
+The largest number is 20'''
