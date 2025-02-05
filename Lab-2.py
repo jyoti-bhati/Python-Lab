6c107a66ad9f06f3206d91fa28f986a56c114419
jyoti-bhati <bhjyoti12@gmail.com>
2025-02-05 16:28:34 +0530

    Add files via upload

---
+++ Lab-2.py
@@ -0,0 +1,12 @@
+'''2.Create a Python program that checks whether a person is eligible to vote
+(18 years or older) based on their age.'''
+
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("Eligible to vote!")
+else:
+    print("Not eligible to vote")
+
+'''output
+Enter your age: 25
+Eligible to vote!'''
