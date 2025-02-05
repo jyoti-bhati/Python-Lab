875cc099fefd54909a5e5bd904a5a31908b6cc61
jyoti-bhati <bhjyoti12@gmail.com>
2025-02-05 17:26:43 +0530

    Add files via upload

---
+++ func_practiceQues.py
@@ -0,0 +1,70 @@
+
+# 1. Function with no arguments and no return value
+def largest_no_args_no_return():
+    a = int(input("Enter 1 number: "))
+    b = int(input("Enter 2 number: "))
+    c = int(input("Enter 3 number: "))
+    largest = max(a, b, c)
+    print("The largest number is:", largest)
+    
+print("Using function with no arguments and no return value:")
+largest_no_args_no_return()
+print(
+
+
+
+
+
+
+
+
+    )
+# 2. Function with arguments and no return value
+def largest_with_args_no_return(a, b, c):
+    largest = max(a, b, c)
+    print("The largest number is:", largest)
+print("\nUsing function with arguments and no return value:")
+largest_with_args_no_return(5, 20, 15)
+
+print(
+
+
+
+
+
+
+
+
+    )
+# 3. Function with no arguments but with a return value
+def largest_no_args_with_return():
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    c = int(input("Enter the third number: "))
+    return max(a, b, c)
+print("\nUsing function with no arguments but with a return value:")
+result = largest_no_args_with_return()
+print("The largest number is:", result)
+print(
+
+
+
+
+
+
+
+
+    )
+# 4. Function with arguments and a return value
+def largest_with_args_with_return(a, b, c):
+    return max(a, b, c)
+print("\nUsing function with arguments and with a return value:")
+result = largest_with_args_with_return(26, 5, 30)
+print("The largest number is:", result)
+
+
+ 
+
+
+
+
