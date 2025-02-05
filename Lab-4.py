f123dcaa5b283e540ee5e1e626ccf66dbdac042d
jyoti-bhati <bhjyoti12@gmail.com>
2025-02-05 16:31:06 +0530

    Add files via upload

---
+++ Lab-4.py
@@ -0,0 +1,14 @@
+'''4. Create a Python program that checks
+if a user-given number is positive, negative, or zero.'''
+
+num = float(input("Enter a number: "))
+if num > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+'''output
+Enter a number: 0.23
+The number is positive.'''
