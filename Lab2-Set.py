66cc6f704cf7000c365578a02192563d5647f22e
jyoti-bhati <bhjyoti12@gmail.com>
2025-03-03 15:08:57 +0530

    Add files via upload

---
+++ Lab2-Set.py
@@ -0,0 +1,31 @@
+#Check if 'apple' is present in the set
+fruits = {'apple', 'banana', 'cherry'}
+if 'apple' in fruits:
+    print("'apple' is present in the set.")
+else:
+    print("'apple' is not present in the set.")
+
+#outpot = 'apple' is present in the set.
+
+
+
+
+
+#Length of a set
+numbers = {10, 20, 30, 40, 50}
+print("The length of the set is:", len(numbers))
+
+#outpot = The length of the set is: 5
+
+
+
+
+
+
+#Remove duplicates from a list
+original_list = [1, 2, 2, 3, 4, 4, 5]
+unique_list = list(set(original_list))
+print("List after removing duplicates:", unique_list)
+
+
+#outpot = List after removing duplicates: [1, 2, 3, 4, 5]
